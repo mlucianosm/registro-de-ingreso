51390e3476ca540ef8dc86e099f8551277a6a96a
marcelo sanchez munoz <mlucianosm@gmail.com>
2019-06-09 16:46:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+        firebase.initializeApp({
+         apiKey: "AIzaSyAp8hM1_awR00OcuSUpWJsa1Fy7pFprQt0",
+         authDomain: "eva2-e7262.firebaseapp.com",
+         databaseURL: "https://eva2-e7262.firebaseio.com",
+         projectId: "eva2-e7262"
+        });
+
+        // Initialize Cloud Firestore through Firebase
+        var db = firebase.firestore();
+
+        function agregardatos(){
+
+            var nombre = document.getElementById("nombre").value;
+            var apellido = document.getElementById("apellido").value;
+            var edad = document.getElementById("edad").value;
+
+            db.collection("usuarios").add({
+                first: nombre,
+                last: apellido,
+                born: edad
+            })
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+
+        }
+
+        function eliminardatos(){
+
+           
+
+        db.collection("usuarios").doc("mCIxBtYSdZ4pmNYNt5CA").delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+    }
+         function todoslosdatos(){
+            db.collection("usuarios").get().then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data().first + "-" + doc.data().last + "-" + doc.data().born);
+                });
+            });
+            }
+        
